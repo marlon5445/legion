c2257e37a8d02f7488ebfb29aa959d52e3633b4f
Marlon Guadalupe <marlon.guadalupe@pucp.edu.pe>
2020-02-12 15:48:28 -0500

    test pagina incio

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <!-- Include meta tag to ensure proper rendering and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Include jQuery Mobile stylesheets -->
+  <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+  <!-- Include the jQuery library -->
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <!-- Include the jQuery Mobile library -->
+  <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+ 
+  
+</head>
+<body>
+<div data-role="page" id="legion">
+  <div data-role="header">
+    <a href="http://hospedajesaunakusi.com/cemduc/retos/list.php" data-ajax="false" class="ui-btn ui-btn-left ui-corner-all ui-shadow ui-icon-home ui-btn-icon-left">Inicio</a>
+    <h1>Legión</h1>
+    <a href="http://hospedajesaunakusi.com/cemduc/retos/report.php" data-ajax="false" class="ui-btn ui-btn-rigth ui-corner-all ui-shadow ui-icon-bars ui-btn-icon-left">Eventos</a>
+  </div>
+
+  <div data-role="main" class="ui-content">
+     
+  </div>
+  <!--<div data-role="footer">
+    <h1>Insert Footer Text Here</h1>
+  </div>-->
+</div>
+
+</body>
+</html>
